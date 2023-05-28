058bcf0d1d44d11053680c7034db410e5f6d94a9
Santiago <32301876+SantiCamm@users.noreply.github.com>
2023-05-28 20:42:29 -0300

    feat: sensors activity (#13)

--- android/app/src/main/java/com/ecowater/MainActivity.java
+++ android/app/src/main/java/com/ecowater/MainActivity.java
@@ -2,13 +2,27 @@ package com.ecowater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btBtn = (Button) findViewById(R.id.button);
+
+        btBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SensorsTest.class));
+            }
+        });
     }
 }
